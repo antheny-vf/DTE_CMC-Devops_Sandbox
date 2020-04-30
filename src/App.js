6aabfdcc4be62aa9c0cac3bb1a7074aa052c4f3e
Antheny Wells <anthenywells@Go-away.local>
2020-04-30 15:03:55 -0400

    master commit test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          The Master
+          Master
         </p>
         <a
           className="App-link"
