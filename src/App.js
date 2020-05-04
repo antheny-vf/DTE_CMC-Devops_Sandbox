5afa9e40bd303901a0f9a9d78658a1085664251e
Antheny Wells <anthenywells@Go-away.lan>
2020-05-04 11:01:59 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -8,11 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-<<<<<<< HEAD
           Master
-=======
-          Develop Test
->>>>>>> develop
         </p>
         <a
           className="App-link"
